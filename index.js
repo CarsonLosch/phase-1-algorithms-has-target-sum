8bb16e0ae135f589ec1bbcd0dfcbcc6737b37979
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-04-17 15:43:03 -0700

    commit tree 5238174e2b36afb4b14abeb75c5ce81c10f77cc2

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // look through each # in array
+  for(let i = 0; i < array.length; i++){
+    // look through rest of array
+    for(let j = i + 1; j < array.length; j++){
+      // check if sum matches target value
+      if ((array[i])+ (array[j]) === target) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+O(n * n + 1 + 1)
+O(n^2 + 2)
+//drop constant
+O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  collection of an Array and a target value are inserted as arguments 
+  [a,b,c,d,e] then we iterate through numbers adding a to b then c etc... and comparing sum to === the target value 
+  if === target value return true
+  else !== return false
+
 */
 
 /*
   Add written explanation of your solution here
+  write a function hasTargetsSum that recieves 2 Arg (array, target)
+  should return true if a pair of numbers === the target value
 */
 
 // You can run `node index.js` to view these console logs
